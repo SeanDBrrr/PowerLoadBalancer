51ea6d4e69c3352dca548a36f79995759431b6f2
Dorinel33 <i493729@fhict.nl>
2023-04-21 14:10:31 +0200

    Added ESP PLB connection code

---
+++ test code/PLB/ESP industry/include/IPLB.h
@@ -0,0 +1,12 @@
+#ifndef IPLB_H
+#define IPLB_H
+
+class IPLB
+{
+public:
+    ~IPLB();
+    
+};
+
+
+#endif
---
+++ test code/PLB/ESP industry/src/main.cpp
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+#include <string.h>
+//#include <ESP32Servo.h>
+#include "EspMQTTClient.h"
+ 
+const char *password = "aquamagic23";
+const char *name = "Kiwy";
+const char *mqtt_module = "Group4-Charger2";
+const char *mqtt_topic = "group4/esp2";
+const char *mqtt_topic_receiver = "group4/esp";
+const char *broker_ip = "192.168.61.23";
+
+const int CONNECTION_LED = 2;
+long previousTime = millis();
+
+EspMQTTClient client
+(
+  name,
+  password,
+  broker_ip,
+  "",
+  "",
+  mqtt_module,
+  1883
+);
+
+EspMQTTClient client1(name, password, broker_ip, mqtt_module, 1883);
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(CONNECTION_LED,OUTPUT);
+  client1.enableHTTPWebUpdater();
+  client1.enableDebuggingMessages();
+}
+
+void onConnectionEstablished()
+{
+  digitalWrite(CONNECTION_LED, HIGH);
+  client1.subscribe(mqtt_topic_receiver, [](const String & topic, const String & payload) {
+    Serial.println(payload);
+  });
+}
+
+void loop()
+{
+  client1.loop();
+
+  if(!client1.isConnected())
+  {
+    digitalWrite(CONNECTION_LED, LOW);
+  }
+
+  if(millis() - previousTime >= 5000)
+  {
+    client1.publish(mqtt_topic, "Hello", 0);
+    previousTime = millis();
+  }
+}

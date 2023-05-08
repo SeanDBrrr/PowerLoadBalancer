3686ca235876c795e39703926e2cf4cf833c4b2f
Sean-David <460282@student.fontys.nl>
2023-05-08 17:28:57 +0200

    Fixed and extended button class

--- test code/Demo 1 Code/Charger/EspCodeDemo1/src/main.cpp
+++ test code/Demo 1 Code/Charger/EspCodeDemo1/src/main.cpp
@@ -6,7 +6,7 @@
 #define BUTTON_PIN 4
 #define CONNECTION_LED 2
 #define REQUEST_POWER_LED 5
-
+int counter = 0;
 const char *name = "Kiwy";
 const char *password = "aquamagic23";
 const char *mqtt_module = "Group4-Charger";
@@ -20,11 +20,13 @@ int helper = 0;
 using namespace std;
 long previousTime = millis();
 Button button(BUTTON_PIN); 
+
 EspMQTTClient client1(name, password, broker_ip, mqtt_module, 1883);
 
 void setup()
 {
   Serial.begin(115200);
+  button.begin();
   pinMode(CONNECTION_LED,OUTPUT);
   pinMode(REQUEST_POWER_LED,OUTPUT);
   client1.enableHTTPWebUpdater();
@@ -45,35 +47,56 @@ void onConnectionEstablished()
   });
 }
 
+
+bool Debounce() {
+  static uint16_t state = 0;
+  state = (state<<1) | digitalRead(BUTTON_PIN) | 0xfe00;
+  Serial.println(state);
+  return (state == 0xff00);
+}
+
 void loop()
 {
   client1.loop();
   
-  if (button.Pressed())
+  // if (button.Pressed())
+  // {
+  //   if(helper == 0)
+  //   {
+  //    // digitalWrite(REQUEST_POWER_LED, HIGH);
+  //     Serial.println("Pressed");
+  //     if(dir == 1)
+  //     {
+  //       client1.publish(mqtt_topic, "requestPower");
+  //     }
+  //     else if(dir == -1)
+  //     {
+  //       client1.publish(mqtt_topic, "requestStop");
+  //     }
+  //     dir *= -1;
+  //     helper = 1;
+  //   }
+  // }
+
+  //button.Toggled();
+   //Serial.println(button.Debounce());
+
+  if (button.singlePress())
   {
-    if(helper == 0)
-    {
-      digitalWrite(REQUEST_POWER_LED, HIGH);
-      Serial.println("Pressed");
-      if(dir == 1)
-      {
-        client1.publish(mqtt_topic, "requestPower");
-      }
-      else if(dir == -1)
-      {
-        client1.publish(mqtt_topic, "requestStop");
-      }
-      dir *= -1;
-      helper = 1;
-    }
-  }
-  else
-  {
-    digitalWrite(REQUEST_POWER_LED, LOW);
-    if(helper == 1)
-    {
-      Serial.println("Released");
-      helper = 0;
-    }
-  }
+    counter++;
+    Serial.println("Request " + String(counter));
+ }
+  // else
+  // {
+  // }
+  //  if (button.Pressed())
+  // {
+  //   digitalWrite(REQUEST_POWER_LED, HIGH);
+
+  // }
+  // else
+  // {
+  //   digitalWrite(REQUEST_POWER_LED, LOW);
+  // }
+
 }

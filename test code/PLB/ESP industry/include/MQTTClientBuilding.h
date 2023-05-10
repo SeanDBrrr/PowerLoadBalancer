19abf9687776c2945169f0f59b2cb875b7090954
Dorinel33 <i493729@fhict.nl>
2023-05-10 13:49:25 +0200

    United classes of MQTT PLB module

--- test code/PLB/ESP industry/include/MQTTClientBuilding.h
+++ test code/PLB/ESP industry/include/MQTTClientBuilding.h
@@ -1,6 +1,15 @@
 #ifndef MQTTCLIENTBUILDING_HPP
 #define MQTTCLIENTBUILDING_HPP
 
+#include <string>
+#include "IBuilding.h"
+#include "EspMQTTClient.h"
+
+using namespace std;
+
+const char *mqtt_topic_solarPower = "group4/powerProduced";
+const char *mqtt_topic_charge = "group4/charge";
+
 class MQTTClientBuilding
 {
 private:
@@ -17,20 +26,10 @@ public:
     void receive();
     void onConnectionSubscribe();
     inline int getSolarPower() { return _totalSolarPower; }
+
+    int calculateSolarPower();
+    void charge(float power);
+    void loop();
 };
 
-MQTTClientBuilding::MQTTClientBuilding()
-{
-}
-
-MQTTClientBuilding::~MQTTClientBuilding()
-{
-}
-
-
-
-
-
-
-
-#endif /* MQTTCLIENTBUILDING_HPP */
+#endif
